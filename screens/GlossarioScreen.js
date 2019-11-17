abc808cd343eaad9e2c9e80912f9cc772f590a59
ytubeth3luz <lucasmasaharu76@gmail.com>
2019-11-17 16:06:10 -0200

    sua mensagem

--- screens/GlossarioScreen.js
+++ screens/GlossarioScreen.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import { Text, Platform, StatusBar, SafeAreaView, ImageBackground } from 'react-native';
+import { Text, Platform, StatusBar, SafeAreaView, ImageBackground, Image, StyleSheet, } from 'react-native';
 import background from '../assets/images/bg_img.png';
 
 export default function GlossarioScreen() {
   return(
     <ImageBackground source={background} style={ {width:'100%', height:'100%'} }>
       <SafeAreaView style= { { flex: 1, paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 0 }}>
-      <Text style={{color:'white'}}>Glossario</Text>
+       <Text style={{color:'white'}}>Glossario</Text>
+        <Image source={require('../assets/images/rotas.png')}/>
+         <Text>    TOP = rota superior
+                   JG/Jungle = caçador, rota da selva
+                   MID = rota do meio
+                   BOT/ADC = rota inferior, carregador de dano físico, atirador
+                   SUP = Suporte, rota inferior</Text>  
+        <Image source={require('../assets/images/invade.png')}/>
+        <Text>Ato onde o time inimigo adentra a selva adversária</Text>
       </SafeAreaView>
-    </ImageBackground>
+         </ImageBackground>
   )
 }
 
