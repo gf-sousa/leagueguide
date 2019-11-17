9f8e27a8b744db43f526c02ec0264b2e026d1b38
Paulo Roberto de Sousa Júnior <PauloSousa@Paulo-Roberto-de-Sousa-Junior.local>
2019-11-17 03:30:11 -0400

    Imagem no Starter (Substituir por gif para testar)

--- screens/StarterScreen.js
+++ screens/StarterScreen.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { View, SafeAreaView, Platform, StatusBar, ImageBackground, Image} from 'react-native';
 import background from '../assets/images/bg_img.png';
+import imageteste from '../assets/images/imgtst.jpg';
 
 export default function StarterScreen() {
   return (
     <ImageBackground source={background} style={ {width:'100%', height:'100%'} }>
     <SafeAreaView style= { { flex: 1, paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 0 }}>
     <View style={{flex:1}}>
-    <Image source={{uri: 'enchttps://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiMkc6Xx_DlAhUDFLkGHcbBC4gQjRx6BAgBEAQ&url=https%3A%2F%2Fgiphy.com%2Fexplore%2Frengar&psig=AOvVaw22WAF2T3Q2adoGSI1-cy3h&ust=1574055945851645urtador.com.br/bjvK2'}} style={{width: '100%', height: '100%'}}/>
+    <Image source={imageteste} style={{width: 300, height: 200, alignSelf: 'center', marginVertical: 20, marginHorizontal: 10}}/>
     </View>
     </SafeAreaView>
     </ImageBackground>
