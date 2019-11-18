76d69139b4a72fec06dd3e01c05e711688368e67
Paulo Roberto de Sousa Júnior <PauloSousa@Paulo-Roberto-de-Sousa-Junior.local>
2019-11-18 15:33:24 -0400

    Imagens no glossario

--- screens/GlossarioScreen.js
+++ screens/GlossarioScreen.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Text, Platform, StatusBar, SafeAreaView, ImageBackground, Image, StyleSheet, } from 'react-native';
+import { View, Text, Platform, StatusBar, SafeAreaView, ImageBackground, Image} from 'react-native';
 import background from '../assets/images/bg_img.png';
+import imga from '../assets/images/rotas.jpg';
+import imgb from '../assets/images/invade.png';
 
 export default function GlossarioScreen() {
   return(
     <ImageBackground source={background} style={ {width:'100%', height:'100%'} }>
       <SafeAreaView style= { { flex: 1, paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 0 }}>
-       <Text style={{color:'white'}}>Glossario</Text>
-        <Image source={require('../assets/images/rotas.png')}/>
-         <Text>    TOP = rota superior
+       
+        <View style={{flex: 1, flexDirection: 'column', }}>
+        <Text style={{color:'white'}}>Glossario</Text>
+        <Image source={imga} style={{width: 100, height: 100}}/>
+         <Text style={{ flex: 1, color: 'white'}}>   
+                   TOP = rota superior
                    JG/Jungle = caçador, rota da selva
                    MID = rota do meio
                    BOT/ADC = rota inferior, carregador de dano físico, atirador
                    SUP = Suporte, rota inferior</Text>  
-        <Image source={require('../assets/images/invade.png')}/>
+        <Image source={imgb} style={{width: 100, height: 100}}/>
         <Text>Ato onde o time inimigo adentra a selva adversária</Text>
+        </View>
       </SafeAreaView>
          </ImageBackground>
   )
