040482f248118654f57c203ed4e5106a432f1a16
Paulo Roberto de Sousa Júnior <PauloSousa@Paulo-Roberto-de-Sousa-Junior.local>
2019-11-19 13:34:26 -0400

    Tela Starter

--- screens/GlossarioScreen.js
+++ screens/GlossarioScreen.js
@@ -14,8 +14,8 @@ export default function GlossarioScreen() {
        
         <Image source={imga} style={{width: 100, height: 100}}/>
          <Text style={{ flex: 1, color: 'white'}}>
-                        Rotas </Text>
-         <Text style={{ flex: 1, color: 'white'}}>   
+                        Rotas:  </Text>
+         <Text style={{ flex: 1, color: 'white', textAlign: 'justify', marginHorizontal: 5}}>   
                    TOP = rota superior
                    JG/Jungle = caçador, rota da selva
                    MID = rota do meio
@@ -23,20 +23,20 @@ export default function GlossarioScreen() {
                    SUP = Suporte, rota inferior</Text>  
         <Image source={imgb} style={{width: 100, height: 100}}/>
        
-        <Text style={{ flex: 1, color: 'white'}}>
-                       Givar }}</Text>
-        <Text style={{ flex: 1, color: 'white'}}> Entregar alguma coisa no jogo, exemplo tem 2 jogadores do time inimigo mortos e vcs startam barão, é melhor eles givar do que ir lá e morrer</Text>
+        <Text style={{ flex: 1, color: 'white', textAlign: 'justify', marginHorizontal: 5}}>
+                       Givar: </Text>
+        <Text style={{ flex: 1, color: 'white', textAlign: 'justify', marginHorizontal: 5}}> Entregar alguma coisa no jogo, exemplo tem 2 jogadores do time inimigo mortos e vcs startam barão, é melhor eles givar do que ir lá e morrer</Text>
 
-        <Text style={{ flex: 1, color: 'white'}}> harass </Text>
-        <Text style={{ flex: 1, color: 'white'}}> É quando alguém fica incomodando um personagem adversário, causando dano nele sempre que possível para deixá-lo desconfortável. Pode ser corporal ou à distância, o importante é ficar fazendo o adversário perder vida sempre que
+        <Text style={{ flex: 1, color: 'white', textAlign: 'justify', marginHorizontal: 5}}> Harass: </Text>
+        <Text style={{ flex: 1, color: 'white', textAlign: 'justify', marginHorizontal: 5}}> É quando alguém fica incomodando um personagem adversário, causando dano nele sempre que possível para deixá-lo desconfortável. Pode ser corporal ou à distância, o importante é ficar fazendo o adversário perder vida sempre que
         possível, seja com auto ataque ou com habilidades. A ideia é tentar fazer o adversário sair da lane ou se sentir intimidado na hora de ir farmar ou destruir alguma coisa.</Text>
         
-        <Text style={{ flex: 1, color: 'white'}}>
-                       Chase }}</Text>
-        <Text style={{ flex: 1, color: 'white'}}> Perseguição - Ficar indo de um alvo pra matar ele. </Text>               
+        <Text style={{ flex: 1, color: 'white', textAlign: 'justify', marginHorizontal: 5}}>
+                       Chase: </Text>
+        <Text style={{ flex: 1, color: 'white', textAlign: 'justify', marginHorizontal: 5}}> Perseguição - Ficar indo de um alvo pra matar ele. </Text>               
         
-        <Text style={{ flex: 1, color: 'white'}}>All win </Text>
-        <Text style={{ flex: 1, color: 'white'}}>Significa você investir com tudo contra seu adversário na lane. Basicamente, gastar tudo que você tem pra garantir kill.</Text>
+        <Text style={{ flex: 1, color: 'white', textAlign: 'justify', marginHorizontal: 5}}>All win: </Text>
+        <Text style={{ flex: 1, color: 'white', textAlign: 'justify', marginHorizontal: 5}}>Significa você investir com tudo contra seu adversário na lane. Basicamente, gastar tudo que você tem pra garantir kill.</Text>
         </View>
       </SafeAreaView>
          </ImageBackground>
--- screens/StarterScreen.js
+++ screens/StarterScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, SafeAreaView, Platform, StatusBar, ImageBackground, Image} from 'react-native';
+import { Text, View, SafeAreaView, Platform, StatusBar, ImageBackground, Image, StyleSheet, FlatList} from 'react-native';
 import background from '../assets/images/bg_img.png';
 import imageteste from '../assets/images/gif-combo.gif';
 
@@ -9,11 +9,40 @@ export default function StarterScreen() {
       <SafeAreaView style= { { flex: 1, paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 0 }}>
         <View style={{flex:1}}>
         <Image source={imageteste} style={{width: 300, height: 200, alignSelf: 'center', marginVertical: 20, marginHorizontal: 10}}/>
+        <View style={styles.container, {flex: 1, alignSelf: 'center'}}>
+        <FlatList
+          data={[
+            { key: 'Tópicos auxiliares:' },
+            { key: '1 - Genero MOBA: Multiplayer Online Battle Arena, duas equipes irão se enfrentar para destruir o Nexus inimigo;' },
+            { key: '2 - O principal mapa de League of Legends se chama Summoners Rift;'},
+            { key: '3 - O jogador deve escolher um campeão para entrar em uma partida gerenciada;' },
+            { key: '4 - Existem mais de 146 campeões disponíveis para jogar;' },
+            { key: '5 - Os jogadores tem disponivel Feitiços de Invocador para poder auxilar seu campeão em partida;' },
+            { key: '6 - Todas as partidas gerenciadas são únicas, seja ela na fila Normal ou Ranqueada;' },
+            { key: '7 - Para obter OURO é necessario causar o ultimo golpe nas tropas' }
+          ]}
+          renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
         </View>
       </SafeAreaView>
     </ImageBackground>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    fontSize: 14,
+    height: 44,
+    color: 'white',
+    textAlign: 'center',
+
+  },
+});
 
 StarterScreen.navigationOptions = {
   header: null,
