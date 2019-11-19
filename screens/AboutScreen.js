514c07910fdbccdd4a9badca96d81d727cfed160
Paulo Roberto de Sousa Júnior <PauloSousa@Paulo-Roberto-de-Sousa-Junior.local>
2019-11-19 17:03:11 -0400

    algo no aboutscreen

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -6,12 +6,12 @@ import logo from '../assets/images/logo.png';
 export default function AboutScreen() {
   return (
     <ImageBackground source={background} style={ {flex: 1} }>
-    <SafeAreaView style= { { flex: 1, paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 0 }}>
-    <View style={{flex: 1, alignSelf: 'center', alignItems: 'center'}}>
-    <Image source={logo} style={{width: 250, height: 100, alignSelf: 'center', marginVertical: 25}}/>
-    <Text style={{flex:1, color: 'white',marginHorizontal: 25, textAlign: 'center'}}>Projeto 'League Guide' tem o proposito de catalogar informações sobre League of Legends</Text>
-    </View>
-    </SafeAreaView>
+     <SafeAreaView style= { { flex: 1, paddingTop: Platform.OS == 'android' ? StatusBar.currentHeight : 0 }}>
+       <View style={{flex: 1, alignSelf: 'center', alignItems: 'center'}}>
+         <Image source={logo} style={{width: 250, height: 100, alignSelf: 'center', marginVertical: 25}}/>
+          <Text style={{flex:1, color: 'white',marginHorizontal: 25, textAlign: 'center'}}>Projeto 'League Guide' tem o proposito de catalogar informações sobre League of Legends.</Text>
+        </View>
+      </SafeAreaView>
     </ImageBackground>
   );
 }
