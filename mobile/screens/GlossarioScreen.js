231aa824bffe6a14cfd84a11cbb6d0abc80da6fa
bielsousa <gabriel.sousa02@hotmail.com>
2019-12-16 20:28:19 -0400

    tentativa de usar strapi.io

--- mobile/screens/GlossarioScreen.js
+++ mobile/screens/GlossarioScreen.js
@@ -7,101 +7,37 @@ import {
   SafeAreaView,
   ImageBackground,
   StyleSheet,
-  ScrollView
+  ScrollView,
+  FlatList
 } from "react-native";
 import background from "../components/Background";
-import { Table, Row, Rows } from "react-native-table-component";
+import Table from '../components/GlossaryTable';
 import Anchor from '../components/LinkAnchor'
 
 export default class GlossarioScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tableHead: ["Termo", "Significado"],
-      tableData: [
-        [
-          "Role",
-          "Termo utilizado para se referir a rota que você está jogando. Por exemplo, minha role é mid."
-        ],
-        ["Top", "Nos referimos a rota do topo"],
-        ["Jungle", "Nos referimos a selva."],
-        ["Mid", "Nos referimos a rota do meio."],
-        ["Bot / Adc", "Nos referimos a rota inferior, e a role de atirador."],
-        ["Sup", "Nos referimos a rota inferior, e a role de suporte."],
-        [
-          "Invade",
-          "É uma ação que um jogador faz, no caso, de entrar na selva inimiga."
-        ],
-        ["Wave", "Nos refirimos a onda de tropas."],
-        [
-          "Push",
-          "É uma ação que um jogador faz, no caso, de empurrar a wave. Essa ação faz com que as tropas avancem mais rápido."
-        ],
-        [
-          "Gank",
-          "É uma ação que um jogador faz, no caso, ele ajuda um companheiro de equipe fazendo uma emboscada para abater o inimigo."
-        ],
-        [
-          "Swap",
-          "É uma ação que a equipe faz, no caso, eles invertem a rota que estão"
-        ],
-        [
-          "Dive",
-          "É uma ação que a equipe faz, no caso, eles 'mergulham' para abater os inimigos de baixo da torre."
-        ],
-        [
-          "Harass",
-          "É uma ação que um jogador faz, no caso, ele irá causar um dano explosivo no inimigo, com a intenção de reduzir sua barra de vida."
-        ],
-        [
-          "Tracking",
-          "Se trata de você reconhecer o posicionamento do inimigo na Névoa de Guerra."
-        ],
-        [
-          "Névoa de Guerra",
-          "É um detalhe do mapa que impede de você enxergar, a não ser que você ou algum aliado esteja para dar visão, ou uma ward, ou uma torre."
-        ],
-        [
-          "Ward",
-          "Ward é o termo para se referir a totem de sentinela. Ela dá visão para sua equipe em uma determinada área na Fog."
-        ],
-        ["Fog", "Termo para se referir a Névoa de Guerra."],
-        [
-          "Pink",
-          "É uma sentinela de controle que pode ser comprada por 75 de ouro, e ela controla uma área revelando totens de sentinelas inimigas e bloqueando elas."
-        ],
-        ["Pathing", "É você ter reconhecimento do caminho que o oponente fez."],
-        [
-          "Shadow",
-          "É o ato de um jogador dar cover na fog enquanto um aliado está fazendo Split. "
-        ],
-        [
-          "Cover",
-          "É uma ação de um jogador dar cobertura para um aliado na rota, objetivo ou split."
-        ],
-        [
-          "Freeze",
-          "É a ação de manipular a wave de maneira que as tropas fiquem se enfrntando em um ponto especifico da rota."
-        ],
-        [
-          "Trade",
-          "É uma decisão em equipe de abandonar um objetivo para pegar outro. Este termo também é utilizado para se referir a uma troca de dano entre dois jogadores."
-        ],
-        [
-          "All win",
-          "Um termo importado do poker, se trata de usar todos os recursos disponíveis para abater o oponente."
-        ],
-        [
-          "Poke",
-          "É a ação de causar pouco de dano no inimigo ao decorrer do tempo com suas habilidades."
-        ],
-        [
-          "Split",
-          "É a ação de um jogador empurrar uma rota lateral para danificar uma torre."
-        ]
-      ]
-    };
+      isLoading: true,
+      dataSource: null
+    }
   };
+
+  componentDidMount(){
+
+    return fetch('http://localhost:1337/')
+    .then( (response)=> response.json())
+    .then( (responseJson) => {
+      this.setState({
+        isLoading: false,
+        dataSource: responseJson.glossarios
+      })
+    })
+    .catch((error) => {
+      console.log(`O erro foi: ${error}`)
+    });
+  }
+
   render() {
     const state = this.state;
     return (
@@ -117,14 +53,15 @@ export default class GlossarioScreen extends React.Component {
         >
           <ScrollView style={styles.scrollView}>
             <View>
-              <Table borderStyle={{ borderWidth: 2, borderColor: "#c8e1ff" }}>
-                <Row
-                  data={state.tableHead}
-                  style={styles.head}
-                  textStyle={styles.text}
-                />
-                <Rows data={state.tableData} textStyle={styles.text} />
-              </Table>
+            <FlatList
+              data={this.state.data}
+              renderItem={({item}) => (
+                <View>
+                  <Text>{item.termo}/</Text>
+              <Text>{item.significado}</Text>
+                </View>
+              )}
+            />
             </View>
             <Anchor href="http://ifms.edu.br/">Conhece algum termo que está faltando? Entre em contato conosco!</Anchor>
           </ScrollView>
